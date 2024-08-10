62f2964d24811138bae7417ea9bcd58d801d07b5
javokhir3414@icloud.com <b35.97-102-222-29@Macbook-Air-2.local>
2024-08-10 20:25:57 +0500

    vercel fix

--- src/components/single-page/SinglePage.jsx
+++ src/components/single-page/SinglePage.jsx
@@ -6,13 +6,13 @@ import star from "../../img/star.svg";
 import Loading from "../loading/Loading";
 
 const SinglePage = () => {
-	// const renderStars = (rating) => {
-	// 	const stars = [];
-	// 	for (let i = 0; i < rating; i++) {
-	// 		stars.push(<img key={i} src={star} alt="star" width={18} height={18} />);
-	// 	}
-	// 	return stars;
-	// };
+	const renderStars = (rating) => {
+		const stars = [];
+		for (let i = 0; i < rating; i++) {
+			stars.push(<img key={i} src={star} alt="star" width={18} height={18} />);
+		}
+		return stars;
+	};
 
 	const [product, productUser] = useState({});
 	const [loading, setLoading] = useState(true);
@@ -36,10 +36,10 @@ const SinglePage = () => {
 					<div className="page__content">
 						<h2 className="page__text">{product.title}</h2>
 						<div className="page__category">{product.category}</div>
-						{/* <div className="page__rating">
+						<div className="page__rating">
 							<span className="stars">{renderStars(product.rating.rate)}</span>{" "}
 							{product.rating.rate}/<span>5</span>
-						</div> */}
+						</div>
 						<span className="line"></span>
 						<p className="page__description">{product.description}</p>
 
